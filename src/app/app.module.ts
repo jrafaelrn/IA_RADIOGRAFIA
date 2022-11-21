d192398b308591eced414d2351ee83a4dce06696
Luiz Fernando <luizfernandossantos909@usp.br>
2022-11-21 20:09:28 -0300

    att2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { TokenInterceptor } from './interceptors/token.interceptors';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ServiceService } from './services/service.service';
 import { LoginComponent } from './login/login.component';
 import { SharedModule } from './shared/SharedModule';
 import { AppMaterialModule } from './shared/app-material/app-material.module';
@@ -26,7 +26,10 @@ import { DashboardModule } from './dashboard/dashboard.module';
     AppMaterialModule,
     DashboardModule
   ],
-  providers: [ServiceService],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
