9089adb5b6be4df4c7ccf9e37f11c12bac5630aa
Luiz Fernando <luizfernandossantos909@usp.br>
2022-11-21 22:30:00 -0300

    as

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  medicoLogin: '1a7b-35-231-64-167.ngrok.io/buscarMedico?crm='
+  medicoLogin: 'https://1a7b-35-231-64-167.ngrok.io/buscarMedico?crm='
 };
 
 /*
