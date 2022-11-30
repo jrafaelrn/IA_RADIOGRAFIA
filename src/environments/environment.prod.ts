7a0360a3605c681d27934984cb7f0ef514b5b438
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-11-29 22:33:02 -0300

    update URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: '9af3-34-82-8-196.ngrok.io',
-  medicoLogin: '9af3-34-82-8-196.ngrok.io/buscarMedico?cpf=',
-  upload: '9af3-34-82-8-196.ngrok.io/cadastrarRadiografia'
+  url: '',
+  medicoLogin: 'http://9af3-34-82-8-196.ngrok.io/buscarMedico?cpf=',
+  upload: 'http://9af3-34-82-8-196.ngrok.io/cadastrarRadiografia'
 };
